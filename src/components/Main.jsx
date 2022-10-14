c2a40a838bc2f62509073bd54d1e3460c043db6a
Violet Cutler <violetcutler@Violets-MacBook-Air.local>
2022-10-14 12:49:01 -0400

    created template for computer monitor display site

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { Navbar } from "./index";
 
 const Main = () => {
   return (
     <div id="main">
-    <div id="navbar">
-      <div>Contact List</div>
+            <Navbar />
+      <h1>I am Main</h1>
+      <section class='paragraph'>
+      <p className="glow">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+      </section>
+      
     </div>
-    <div id="container">
-      {/* your components here */}
-    </div>
-  </div>
   );
 };
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div id="navbar">
+    <h2>I am Navbar</h2>
+  </div>;
+};
+
+export default Navbar;
